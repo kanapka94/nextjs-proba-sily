7e1ee19441d8b88dc900e84b8edc25a6692ceccb
Adam <rumpelteam@gmail.com>
2021-02-27 17:06:53 +0100

    Started implement dynamic routes

---
+++ pages/posts/[id].js
@@ -0,0 +1,24 @@
+import Layout from '../../components/layout'
+
+import { getAllPostsIds } from '../../lib/posts'
+
+export default function Post() {
+    return <Layout></Layout>
+}
+
+export async function getStaticPaths() {
+    const paths = getAllPostsIds()
+
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps({ param }) {
+    return {
+        props: {
+            // 
+        }
+    }
+}
