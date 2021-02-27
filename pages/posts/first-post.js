25c47568fe76c6a1ed340a58c0287e451fe3201c
Adam <rumpelteam@gmail.com>
2021-02-27 10:46:03 +0100

    Added utils css

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,18 @@
 import Link from 'next/link'
+import Head from 'next/head'
+
+import Layout from '../../components/layout'
 
 export default function FirstPost() {
     return (
-        <>
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
             <h1>First Page</h1>
             <Link href="/">
                 <a>back to home</a>
             </Link>
-        </>
+        </Layout>
     )
 }
